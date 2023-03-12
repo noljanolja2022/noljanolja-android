239d1564d5df12d760352167e9a42c43aa92ccea
14H <doduchieu.kstn@gmail.com>
2023-03-12 11:10:32 +0700

    add try catch

--- app/src/main/java/com/noljanolja/android/util/Phone.kt
+++ app/src/main/java/com/noljanolja/android/util/Phone.kt
@@ -5,13 +5,17 @@ import android.telephony.TelephonyManager
 import com.google.i18n.phonenumbers.PhoneNumberUtil
 
 fun formatPhoneNumber(phoneNumber: String?, context: Context): String {
-    val countryCode = getCountryCode(context).uppercase()
-    val phoneUtil = PhoneNumberUtil.getInstance()
-    val numberProto = phoneUtil.parse(phoneNumber, countryCode)
-    return if (phoneUtil.isValidNumber(numberProto)) {
-        phoneUtil.format(numberProto, PhoneNumberUtil.PhoneNumberFormat.E164)
-    } else {
-        null
+    return try {
+        val countryCode = getCountryCode(context).uppercase()
+        val phoneUtil = PhoneNumberUtil.getInstance()
+        val numberProto = phoneUtil.parse(phoneNumber, countryCode)
+        if (phoneUtil.isValidNumber(numberProto)) {
+            phoneUtil.format(numberProto, PhoneNumberUtil.PhoneNumberFormat.E164)
+        } else {
+            phoneNumber
+        }
+    } catch (e: Exception) {
+        phoneNumber
     }.orEmpty()
 }
 
