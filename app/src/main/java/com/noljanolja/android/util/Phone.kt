561ec85cca8355c07caad20cadb8cbcd19b3ae44
Đỗ Đức Hiếu <doduchieu.kstn@gmail.com>
2023-03-12 22:55:05 +0700

    Test/1.0.1 (#98)
    * Update version 1.0.1 with format phone
    
    * add try catch
    
    * update adeepp to noljanolja

--- app/src/main/java/com/noljanolja/android/util/Phone.kt
+++ app/src/main/java/com/noljanolja/android/util/Phone.kt
@@ -1,16 +1,26 @@
 package com.noljanolja.android.util
 
 import android.content.Context
-import android.telephony.PhoneNumberUtils
+import android.telephony.TelephonyManager
+import com.google.i18n.phonenumbers.PhoneNumberUtil
 
-fun String.formatPhone(context: Context): String {
-    val phone = try {
-        PhoneNumberUtils.formatNumberToE164(
-            this,
-            context.getLocalLocale().uppercase()
-        ) ?: this
+fun formatPhoneNumber(phoneNumber: String?, context: Context): String {
+    return try {
+        val countryCode = getCountryCode(context).uppercase()
+        val phoneUtil = PhoneNumberUtil.getInstance()
+        val numberProto = phoneUtil.parse(phoneNumber, countryCode)
+        if (phoneUtil.isValidNumber(numberProto)) {
+            phoneUtil.format(numberProto, PhoneNumberUtil.PhoneNumberFormat.E164)
+        } else {
+            phoneNumber
+        }
     } catch (e: Exception) {
-        this
-    }
-    return phone
+        phoneNumber
+    }.orEmpty()
+}
+
+fun getCountryCode(context: Context): String {
+    val countryCodeValue =
+        (context.getSystemService(Context.TELEPHONY_SERVICE) as? TelephonyManager)?.networkCountryIso
+    return countryCodeValue ?: "KR"
 }
