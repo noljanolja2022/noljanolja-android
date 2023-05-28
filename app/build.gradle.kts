51b6ef144b84a2a56e1ba3a8f8efac6a92692a7e
14H <do.duc.hieu@linecorp.com>
2023-05-28 21:04:04 +0700

    Release version 1.0.15

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.noljanolja.app.android"
         minSdk = 21
         targetSdk = 33
-        versionCode = 19
-        versionName = "1.0.14"
+        versionCode = 20
+        versionName = "1.0.15"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
