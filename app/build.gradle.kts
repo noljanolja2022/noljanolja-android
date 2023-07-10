29bae81e7c711d8c7c901f3d91d352adda7666d3
Đỗ Đức Hiếu <doduchieu.kstn@gmail.com>
2023-07-10 21:39:18 +0700

    Release version 1.0.21 (#192)
    Co-authored-by: 14H <do.duc.hieu@linecorp.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.noljanolja.app.android"
         minSdk = 21
         targetSdk = 33
-        versionCode = 24
-        versionName = "1.0.20"
+        versionCode = 25
+        versionName = "1.0.21"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
