97866045206bb0bb03060bcda776105ee4ed19bd
Đỗ Đức Hiếu <doduchieu.kstn@gmail.com>
2023-07-30 21:45:41 +0700

    Release new app name and logo (#198)
    Co-authored-by: 14H <do.duc.hieu@linecorp.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.noljanolja.app.android"
         minSdk = 21
         targetSdk = 33
-        versionCode = 26
-        versionName = "1.0.23"
+        versionCode = 27
+        versionName = "1.0.24"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
