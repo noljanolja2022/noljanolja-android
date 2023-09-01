05267a3c1ef134541bf916d3cf304c208ce3dae9
14H <do.duc.hieu@linecorp.com>
2023-09-01 09:14:07 +0700

    Release 1.0.31

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.noljanolja.app.android"
         minSdk = 21
         targetSdk = 33
-        versionCode = 33
-        versionName = "1.0.30"
+        versionCode = 34
+        versionName = "1.0.31"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
