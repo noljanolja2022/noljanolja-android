7e6b1c31b28f58c0cdc601b189e54c6664beb34b
14H <do.duc.hieu@linecorp.com>
2023-09-14 23:15:19 +0700

    Release new pip

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.noljanolja.app.android"
         minSdk = 21
         targetSdk = 33
-        versionCode = 35
-        versionName = "1.0.32"
+        versionCode = 36
+        versionName = "1.0.33"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
