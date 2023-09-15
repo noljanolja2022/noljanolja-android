2ae43ada6d389c56a884b241e6cbd25ad43c7e5c
Đỗ Đức Hiếu <doduchieu.kstn@gmail.com>
2023-09-15 09:47:59 +0700

    Release ver 1.0.33 (#216)
    * Release ver 1.0.32
    
    * Release new pip
    
    ---------
    
    Co-authored-by: 14H <do.duc.hieu@linecorp.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.noljanolja.app.android"
         minSdk = 21
         targetSdk = 33
-        versionCode = 34
-        versionName = "1.0.31"
+        versionCode = 36
+        versionName = "1.0.33"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
