879639ead9fcac5b79f02efaf89e68151765bd8c
Đỗ Đức Hiếu <doduchieu.kstn@gmail.com>
2023-09-23 22:32:25 +0700

    Release new pip in app (#220)
    Co-authored-by: 14H <do.duc.hieu@linecorp.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.noljanolja.app.android"
         minSdk = 21
         targetSdk = 33
-        versionCode = 37
-        versionName = "1.0.34"
+        versionCode = 38
+        versionName = "1.0.35"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
