f4622c1a5124fc8cb83f04c4249c66685c491c8d
14H <do.duc.hieu@linecorp.com>
2023-11-27 17:12:12 +0700

    Release version 1.1.23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.ppnyy.nolgobulja"
         minSdk = 21
         targetSdk = 33
-        versionCode = 66
-        versionName = "1.1.22"
+        versionCode = 67
+        versionName = "1.1.23"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
