c71829d2c93e9938394fe6eb20d1a907b6bb7312
Thanh Tuyen <dttuyen260701@gmail.com>
2023-12-16 00:23:07 +0700

    release 1.1.26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.ppnyy.nolgobulja"
         minSdk = 21
         targetSdk = 33
-        versionCode = 69
-        versionName = "1.1.25"
+        versionCode = 70
+        versionName = "1.1.26"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
