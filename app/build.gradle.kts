eb3352e80d68509ae4a36ea5acc1f08e75a8c6dc
Thanh Tuyen <dttuyen260701@gmail.com>
2023-12-19 01:20:20 +0700

    release 1.1.28

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.ppnyy.nolgobulja"
         minSdk = 21
         targetSdk = 33
-        versionCode = 71
-        versionName = "1.1.27"
+        versionCode = 72
+        versionName = "1.1.28"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
