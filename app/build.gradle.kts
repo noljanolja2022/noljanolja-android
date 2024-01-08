f5cffaaa0975198bb37e290e1b80c1d9174a7bcd
Thanh Tuyen <dttuyen260701@gmail.com>
2024-01-09 00:45:45 +0700

    release 1.1.41 in firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
         applicationId = "com.ppnyy.nolgobulja"
         minSdk = 21
         targetSdk = 33
-        versionCode = 57
-        versionName = "1.1.11"//"1.1.40"
+        versionCode = 58
+        versionName = "1.1.11"//"1.1.41"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
