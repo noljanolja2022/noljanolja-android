02739a6eb2f05c3bc5bd34f05b6a14900b9a747c
Thanh Tuyen <dttuyen260701@gmail.com>
2024-01-12 01:37:57 +0700

    release 1.1.43

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 21
         targetSdk = 33
         versionCode = 60
-        versionName = "1.1.11"//"1.1.43"
+        versionName = "1.1.43"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
