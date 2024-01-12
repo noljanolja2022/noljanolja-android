b65e7ec760450a56cc7334a680704241d5d32a17
Thanh Tuyen <dttuyen260701@gmail.com>
2024-01-13 01:30:20 +0700

    release 1.1.44

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 21
         targetSdk = 33
         versionCode = 61
-        versionName = "1.1.11"//"1.1.44"
+        versionName = "1.1.44"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
