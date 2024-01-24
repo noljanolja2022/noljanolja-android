889304706f74483e8bb70255f2c6e35a6befb1d8
Thanh Tuyen <dttuyen260701@gmail.com>
2024-01-25 00:48:39 +0700

    release 1.1.49 in firebase

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 21
         targetSdk = 33
         versionCode = 66
-        versionName = "1.1.12"//"1.1.49"
+        versionName = "1.1.49"
         testInstrumentationRunner = "com.noljanolja.android.InstrumentationTestRunner"
         multiDexEnabled = true
     }
